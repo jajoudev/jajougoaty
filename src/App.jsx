c7fbb7ef0ecf079f9e469fdbfce21e7cf3efb9a3
Jajou 猫 <jahelgithub64@gmail.com>
2025-10-15 10:26:41 +0200

    Welcome Page Functionnal !

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react"
+
 import Profile from "./components/Profile"
 import Links from "./components/Links"
+import WelcomePage from "./components/WelcomePage"
 
 import ProfileImg from "../public/images/profile.jpg"
 
 export default function App() {
+  const [showElement, setShowElement] = useState(false)
+
   return (
     <>
-    
-      <main className="flex flex-col items-center justify-center min-h-screen">
-        <Profile url={ProfileImg} />
-        <Links/>
-      </main>
+      {!showElement ? (
+        <WelcomePage onClick={() => setShowElement(true)}/>
+      ) : (
+        <main className="flex flex-col items-center justify-center min-h-screen">
+          <Profile url={ProfileImg} />
+          <Links />
+        </main>
+      )}
     </>
   )
 }
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,7 +1,8 @@
-export default function WelcomePage() {
+export default function WelcomePage({ onClick }) {
     return (
-        <div>
-            <h1>Welcome to my page !</h1>
+        <div className="min-h-screen flex flex-col justify-center items-center w-full bg-blue-950" onClick={onClick}>
+            <h1 className="text-blue-400 text-center font-bold text-9xl mb-8">Welcome to my page !</h1>
+            <p className="text-center text-2xl text-white">Click anywhere to continue :D</p>
         </div>
     )
 }
